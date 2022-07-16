9a8671fcf8f8c6a7dd07a5fa62e5ff84be1dadbf
AmirHmn <amir.hosein6979@gmail.com>
2022-07-16 13:20:27 +0430

    create student model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=10, unique=True)
+
