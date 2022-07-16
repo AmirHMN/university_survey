e3591217b40db950910e5aaca8a052fcf6be6d4f
AmirHmn <amir.hosein6979@gmail.com>
2022-07-16 14:16:17 +0430

    create survey , question and choice model

--- core/models.py
+++ core/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+
+class Survey(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    question_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
